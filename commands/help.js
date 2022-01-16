1a453ad9e94d11b431f3287c3e227ea6c4ae27f1
Bayupaguse1717 <97423201+Bayupaguse1717@users.noreply.github.com>
2022-01-16 19:16:50 +0000

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -14,7 +14,7 @@ exports.run = async (client, message) => {
    np\`\` - see now playing song
    lyrics\`\` - get lyrics of current song
    shuffle\`\` - shuffle and randomize the queue
-   invite\`\` - get invite link for the bot
+   Help\`\` - get invite link for the bot
    loop\`\` - enable / disable loop for the currently playing song
    remove <Target number>\`\` - remove a song from the queue
    help\`\` - to see this command`;
@@ -27,11 +27,13 @@ exports.run = async (client, message) => {
   message.channel.send(
     new MessageEmbed()
       .setAuthor(
-        "MusicBot Commands Help",
+        "Bloverz Music Commands Help",
         "https://img.icons8.com/color/2x/services--v2.gif"
       )
       .setColor("FFFBFB")
       .setTimestamp()
       .setDescription(revised)
+      .addField(`Use the following link to add BloverzMusic to your discord server`, 'https://discord.com/api/oauth2/authorize?client_id=932326674245251092&permissions=8&scope=bot', true)
+
   );
 };
